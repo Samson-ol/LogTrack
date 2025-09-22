d6a9ded2672a70b0fb39128ee0bf959c93bc4637
Samson Oloyede <oloyedes212@gmail.com>
2025-09-22 19:02:10 +0100

    COLLECT STATIC FILES

--- LOGTRACK/settings.py
+++ LOGTRACK/settings.py
@@ -14,6 +14,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,6 +57,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "LOGTRACK.urls"
+
+# import dj_database_url
+# import os
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+# }
 
 import os
 TEMPLATES = [
