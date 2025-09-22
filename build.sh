f7f8fcf96a472c9d9a070971347b25b97493f978
Samson Oloyede <oloyedes212@gmail.com>
2025-09-23 00:42:04 +0100

    Restore build.sh script with build process and static file collection

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Starting build process..."
+pip install -r requirements.txt
+
+echo "Running collectstatic..."
+python manage.py collectstatic --noinput
+
+echo "Static files collected to:"
+ls -la staticfiles_build/static
