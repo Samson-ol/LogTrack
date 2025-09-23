3e6d90017726a173bfa79f78c11107d113a69609
Samson Oloyede <oloyedes212@gmail.com>
2025-09-23 08:35:38 +0100

    Refactor build.sh and update vercel.json for improved build process and configuration

--- build.sh
+++ build.sh
@@ -1,28 +1,13 @@
 # #!/bin/bash
-# echo "Starting build process..."
-# pip install -r requirements.txt
+echo "Starting build process..."
+pip install -r requirements.txt
 
-# echo "Running collectstatic..."
-# python manage.py collectstatic --noinput
+echo "Running collectstatic..."
+python manage.py collectstatic --noinput
 
-# echo "Static files collected to:"
-# ls -la staticfiles_build/static
+echo "Static files collected to:"
+ls -la staticfiles_build/static
 
-#!/bin/bash
 
-# Build the project
-echo "Building the project..."
 
-# Install project dependencies
-python3 -m pip install -r requirements.txt
 
-# Run database migrations
-echo "Make Migrations..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
-# Collect static files
-echo "Collect Static..."
-python3 manage.py collectstatic --noinput --clear
-
-echo "Build finished successfully."
